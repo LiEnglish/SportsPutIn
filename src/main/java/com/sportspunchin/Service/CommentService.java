bebeb119a6a7096371304503dc6e3db325cd5779
LiEnglish <2961505633@qq.com>
2022-06-11 20:45:47 +0800

    后补

--- src/main/java/com/sportspunchin/Service/CommentService.java
+++ src/main/java/com/sportspunchin/Service/CommentService.java
@@ -2,9 +2,11 @@ package com.sportspunchin.Service;
 
 
 import com.sportspunchin.pojo.Comment;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CommentService {
 
     List<Comment> findmyAllcomment();
--- src/main/java/com/sportspunchin/Service/TUserService.java
+++ src/main/java/com/sportspunchin/Service/TUserService.java
@@ -2,9 +2,11 @@ package com.sportspunchin.Service;
 
 
 import com.sportspunchin.pojo.TUser;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface TUserService {
     List<TUser> findAlluser();
 }
